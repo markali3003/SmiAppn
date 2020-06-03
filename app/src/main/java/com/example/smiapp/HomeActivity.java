40fa6a543b16d4ecfed5f1dafada870473b223fb
AYYOUB <alimark3003@gmail.com>
2020-06-03 23:12:06 +0100

    moi

--- app/src/main/java/com/example/smiapp/HomeActivity.java
+++ app/src/main/java/com/example/smiapp/HomeActivity.java
@@ -44,7 +44,7 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void S6(View view) {
-        globalv.setSemestre("S6");
+        globalv.setSemestre("Sesstre 6");
         startActivity(new Intent(HomeActivity.this, ModelActivity.class));
     }
 
